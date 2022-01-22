fbf7da0ac9cd85e713759b2a0177fe7f8e0b32b3
Administrator <17543299@qq.com>
2022-01-22 21:18:36 +0800

    2022012219461

--- src/main/java/com/siren/entity/JPAUser.java
+++ src/main/java/com/siren/entity/JPAUser.java
@@ -2,13 +2,45 @@ package com.siren.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Created by Administrator on 22-1-22.
  */
 @Data
+@Entity(name = "user")
 public class JPAUser {
-
+    @Id
+    @Column
     private Integer id;
+    @Column
     private String username;
+    @Column
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
